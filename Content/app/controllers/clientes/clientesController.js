8fd69aaf5d32b02890ae6048dd8d894a6c8a5b71
roman0997 <roman_0906@hotmail.com>
2016-02-18 20:51:01 -0500

    cambios

---
+++ Content/app/controllers/clientes/clientesController.js
@@ -0,0 +1,72 @@
+﻿(function () {
+    'use strict';
+
+
+    angular.module("moneyPointsApp").controller('clientesController', ['$http', '$scope', '$rootScope', 'clientesService', clientesController]);
+
+    function clientesController($http, $scope, $rootScope, clientesService) {
+
+        $scope.clientesGridOptions = {
+            dataSource: {
+                type: "json",
+                transport: {
+                    read: {
+                        url: $rootScope.baseAddress + "/api/clientes",
+                        dataType: "json",
+                        beforeSend: $rootScope.beforeSendRequest
+                    },
+                    parameterMap: function (options, type) {
+                        var paramMap = kendo.data.transports.odata.parameterMap(options);
+                        delete paramMap.$inlinecount; // <-- quitar parametro inlinecount
+                        return paramMap;
+                    }
+                },
+                pageSize: 10,
+                serverPaging: true,
+                serverSorting: true,
+                serverFiltering: true,
+            },
+            sortable: true,
+            pageable: true,
+            filterable: {
+                field: "Tercero.Nombre",
+                operator: "contains"
+            },
+            scrollable: {
+                virtual: true
+            },
+            schema: {
+                data: function (data) {
+                    return data;
+                },
+                total: function (data) {
+                    return 6;
+                },
+                model: {
+                    fields: {
+                        "Tercero.Nombre": { type: "string" },
+                        "Tercero.Identificacion": { type: "number" },
+                        "Tercero.Telefono": { type: "string" },
+                    }
+                }
+            },
+            columns: [{
+                field: "Tercero.Nombre",
+                title: "Razón Social"
+            },
+            {
+                field: "Tercero.Identificacion",
+                title: "Nit"
+            },
+            {
+                field: "Tercero.Telefono",
+                title: "Teléfono"
+            }],
+            template: kendo.template($("#tmpClientes").html()),
+        };
+
+
+    };
+
+
+})();
---
+++ Content/app/controllers/login/loginController.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+angular.module('moneyPointsApp')
+
+.controller('loginController',
+    ['$scope', '$rootScope', '$location', 'authenticationService', 'authorizationService', 'Base64', '$http', 'tercerosService',
+    function ($scope, $rootScope, $location, authenticationService, authorizationService, Base64, $http, tercerosService) {
+
+     
+        $scope.login = function () {
+
+            kendo.ui.progress($("#form"), true);
+            $scope.dataLoading = true;
+            $scope.error = "";
+            authenticationService.login($scope.username, $scope.password, function (response) {
+                if (!response.success) {
+                    new PNotify({
+                        text: 'Error: ' + response.Message,
+                        type: 'danger',
+                        delay: 3000
+                    });
+                    $scope.dataLoading = false;
+                    kendo.ui.progress($("#form"), false);
+                }
+                $scope.dataLoading = false;
+                kendo.ui.progress($("#form"), false);
+            });
+        };
+    }]);
